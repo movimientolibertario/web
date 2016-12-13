d33fed313dddd72aa770aa0f5f3e256b8eb5fe12
Jose-Luis Rivas <me@ghostbar.co>
2016-12-13 17:33:05 -0500

    sticky menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,21 @@
+var h = document.getElementById("menu");
+var stuck = false;
+var stickPoint = getDistance();
 
+function getDistance() {
+  var topDist = h.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(e) {
+  var distance = getDistance() - window.pageYOffset;
+  var offset = window.pageYOffset;
+  if ( (distance <= 0) && !stuck) {
+    h.style.position = 'fixed';
+    h.style.top = '0px';
+    stuck = true;
+  } else if (stuck && (offset <= stickPoint)){
+    h.style.position = 'static';
+    stuck = false;
+  }
+}
